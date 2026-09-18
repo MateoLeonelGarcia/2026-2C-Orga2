a545ac1c22e6d0123a044e315e4a0517e33b22b3
Mateo <garoia2006@gmail.com>
2026-09-17 23:55:44 -0300

    ejercicio alineacioon estructuras

--- Guía de ASM 2c2026/src/3 - Alineación y Estructuras/Estructuras.asm
+++ Guía de ASM 2c2026/src/3 - Alineación y Estructuras/Estructuras.asm
@@ -2,19 +2,19 @@
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-NODO_OFFSET_NEXT EQU 8
-NODO_OFFSET_CATEGORIA EQU 8
-NODO_OFFSET_ARREGLO EQU 8
-NODO_OFFSET_LONGITUD EQU 8
-NODO_SIZE EQU 8
-PACKED_NODO_OFFSET_NEXT EQU 8
-PACKED_NODO_OFFSET_CATEGORIA EQU 8
-PACKED_NODO_OFFSET_ARREGLO EQU 8
-PACKED_NODO_OFFSET_LONGITUD EQU 8
-PACKED_NODO_SIZE EQU 8
-LISTA_OFFSET_HEAD EQU 8
+NODO_OFFSET_NEXT EQU 0;;12
+NODO_OFFSET_CATEGORIA EQU 8;;1
+NODO_OFFSET_ARREGLO EQU 16;;8
+NODO_OFFSET_LONGITUD EQU 24;;4
+NODO_SIZE EQU 32;8
+PACKED_NODO_OFFSET_NEXT EQU 0;;8
+PACKED_NODO_OFFSET_CATEGORIA EQU 8;;8
+PACKED_NODO_OFFSET_ARREGLO EQU 9;;8
+PACKED_NODO_OFFSET_LONGITUD EQU 17;;8
+PACKED_NODO_SIZE EQU 21;;8
+LISTA_OFFSET_HEAD EQU 0;;8
 LISTA_SIZE EQU 8
-PACKED_LISTA_OFFSET_HEAD EQU 8
+PACKED_LISTA_OFFSET_HEAD EQU 0;;8
 PACKED_LISTA_SIZE EQU 8
 
 ;########### SECCION DE DATOS
@@ -29,12 +29,70 @@ global cantidad_total_de_elementos_packed
 
 ;########### DEFINICION DE FUNCIONES
 ;extern uint32_t cantidad_total_de_elementos(lista_t* lista);
-;registros: lista[?]
+;registros: lista[RDI] el primer valor es un puntero a un nodo_t
 cantidad_total_de_elementos:
+	push rbp
+	mov rbp, rsp
+	; int i = 0;
+	xor rcx,rcx  ;; mi contador rcx=0
+	xor r8,r8
+	xor r9,r9 ;; sum =0
+    ; nodo_t *nodotmp = lista.head;
+	mov r10, [rdi] ;;estaria agarrando el puntero al nodo head?
+; while (nodotmp != NULL)
+    ; {
+	
+.loop:
+	cmp r10, 0x0 ;;null seria la direccion 0x0000 0000 0000 0000
+	je .finloop
+;     i++;
+	inc rcx
+	;; sum += nodo.longitud
+	mov r9, [r10 + NODO_OFFSET_LONGITUD]
+	add r8,r9
+	 ;     nodotmp = nodotmp->next;
+	mov r11,[r10]
+	mov r10,r11
+	jmp .loop
+.finloop:
+
+
+    ; return i;
+	mov rax,r8
+	pop rbp
 	ret
 
 ;extern uint32_t cantidad_total_de_elementos_packed(packed_lista_t* lista);
 ;registros: lista[?]
 cantidad_total_de_elementos_packed:
+	push rbp
+	mov rbp, rsp
+	; int i = 0;
+	xor rcx,rcx  ;; mi contador rcx=0
+	xor r8,r8
+	xor r9,r9 ;; sum =0
+    ; nodo_t *nodotmp = lista.head;
+	mov r10, [rdi] ;;estaria agarrando el puntero al nodo head?
+; while (nodotmp != NULL)
+    ; {
+	
+.loop:
+	cmp r10, 0x0 ;;null seria la direccion 0x0000 0000 0000 0000
+	je .finloop
+;     i++;
+	inc rcx
+	;; sum += nodo.longitud
+	mov r9D, [r10 + PACKED_NODO_OFFSET_LONGITUD]
+	add r8D,r9D
+	 ;     nodotmp = nodotmp->next;
+	mov r11,[r10]
+	mov r10,r11
+	jmp .loop
+.finloop:
+
+
+    ; return i;
+	mov eax,r8d
+	pop rbp
 	ret
 
--- Guía de ASM 2c2026/src/3 - Alineación y Estructuras/Estructuras.c
+++ Guía de ASM 2c2026/src/3 - Alineación y Estructuras/Estructuras.c
@@ -1,3 +1,46 @@
 #include "Estructuras.h"
 
 /* Pueden programar alguna rutina auxiliar acá */
+// static uint32_t x[4];
+// static double f[4];
+
+// void shuffle(uint32_t max)
+// {
+//     for (int i = 0; i < 10; i++)
+//     {
+//         x[i] = (uint32_t)rand() % max;
+//         f[i] = ((float)rand() / (float)(RAND_MAX)) * max;
+//     }
+// }
+// int main()
+// {
+//     nodo_t *array[4];
+//     lista_t lista;
+//     for (int i = 0; i < 100; i++)
+//     {
+//         shuffle(1000);
+
+//         for (int j = 0; j < 4; j++)
+//         {
+//             array[j] = calloc(1, sizeof(nodo_t));
+//             array[j]->longitud = x[j];
+//         }
+
+//         for (int j = 0; j < 4 - 1; j++)
+//         {
+//             array[j]->next = array[j + 1];
+//         }
+
+//         lista_t lista;
+//         lista.head = array[0];
+//     }
+
+//     int i = 0;
+//     nodo_t *nodotmp = lista.head;
+//     while (nodotmp != NULL)
+//     {
+//         i++;
+//         nodotmp = nodotmp->next;
+//     }
+//     return i;
+// }
