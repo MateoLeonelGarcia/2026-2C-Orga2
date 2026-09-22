c8a8769743f66eb5f517af4d82ede4a58291096d
Mateo <garoia2006@gmail.com>
2026-09-22 16:48:33 -0300

    added ej 6

--- Guía de ASM 2c2026/src/6 - Debugging/Debugging.asm
+++ Guía de ASM 2c2026/src/6 - Debugging/Debugging.asm
@@ -11,79 +11,102 @@ FALSE EQU 0
 ; Marca un ejercicio como hecho
 TRUE  EQU 1
 
-ITEM_OFFSET_NOMBRE EQU 9
-ITEM_OFFSET_ID EQU 16
-ITEM_OFFSET_CANTIDAD EQU 24
+ITEM_OFFSET_NOMBRE EQU 0;;9
+ITEM_OFFSET_ID EQU 12;;16
+ITEM_OFFSET_CANTIDAD EQU 16;;24
 
 POINTER_SIZE EQU 4
-UINT32_SIZE EQU 8
+UINT32_SIZE EQU 4
 
 ; Marcar el ejercicio como hecho (`true`) o pendiente (`false`).
 
 global EJERCICIO_1_HECHO
-EJERCICIO_1_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_1_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 global EJERCICIO_2_HECHO
-EJERCICIO_2_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_2_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 global EJERCICIO_3_HECHO
-EJERCICIO_3_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_3_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 global EJERCICIO_4_HECHO
-EJERCICIO_4_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_4_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 global ejercicio1
 ejercicio1:
-	add edi, ecx
-	add edi, edx
-    add edi, ebx
-    add edi, r9d
-	mov eax, edi
+	add rdi, rcx
+	add rdi, rdx
+    add rdi, rsi
+    add rdi, r8
+	mov rax, rdi
 	ret
 
 global ejercicio2
 ejercicio2:
-	mov [rdi+ITEM_OFFSET_ID], rsi
-	mov [rdi+ITEM_OFFSET_CANTIDAD], rdx
-	call strcpy 
+	push rbp
+	mov rbp, rsp
+	mov [rdi+ITEM_OFFSET_ID], esi
+	mov [rdi+ITEM_OFFSET_CANTIDAD], edx
+	mov esi, ecx
+	call strcpy
+	pop rbp
 	ret
 
 
 global ejercicio3
 ejercicio3:
-	cmp rsi, 0
+	push rbp
+	mov rbp, rsp
+
+	cmp esi, 0
 	je .vacio
 	
 	mov rcx, rdi ; array
 	mov r8, 0 ; sumatoria
 	mov r9, 0 ; i
+	mov r10, rsi ;; tamanio original del arreglo
 
 	.loop:
 	mov rdi, r8
-	mov rsi, [rcx + r9*4]
+	mov esi, [rcx + r9*4]
+	push r10
+	push rdx
+	push rcx
+	push r9
+	push r8
+	sub rsp,8
 
 	call rdx
 
+	add rsp,8
+	pop r8
+	pop r9
+	pop rcx
+	pop rdx
+	pop r10
+
 	add r8, rax
-	mov rax, r8
-
 	inc r9
-	cmp r9, rsi
-	je .end
+	cmp r9, r10
+	jl .loop
 
-	jmp .loop
+	mov rax, r8
+	pop rbp
+	ret
 
 	.vacio:
 	mov rax, 64
-
-	.end:
+	pop rbp
 	ret
 
 global ejercicio4
 ejercicio4:
+	push rbp
+	mov rbp,rsp
+
 	mov r12, rdi
-	mov r13, rsi
-	mov r14, rdx
+	mov r13d, esi
+	mov r14d, edx
 
 	xor rdi, rdi
 	mov eax, UINT32_SIZE
@@ -96,12 +119,12 @@ ejercicio4:
 	xor rbx, rbx
 	.loop:
 	
-	cmp rbx, r13
+	cmp ebx, r13d
 	je .end
 
 	mov r8, [r12+rbx*POINTER_SIZE]
 	mov r9d, [r8]
-	mov rax, r14
+	mov eax, r14d
 	mul r9d
 	mov [r15+rbx*UINT32_SIZE], eax
 	
@@ -113,4 +136,5 @@ ejercicio4:
 
 	.end:
 	mov rax, r15
+	pop rbp
 	ret
--- Guía de ASM 2c2026/src/6 - Debugging/tester.c
+++ Guía de ASM 2c2026/src/6 - Debugging/tester.c
@@ -10,10 +10,10 @@
 // Cuenta cuántos tests corrieron exitosamente.
 uint64_t successful_tests = 0;
 
-//Cuenta cuántos tests test fallaron.
+// Cuenta cuántos tests test fallaron.
 uint64_t failed_tests = 0;
 
-//El mensaje [DONE] escrito en verde.
+// El mensaje [DONE] escrito en verde.
 #define DONE "[\033[32;1mDONE\033[0m] "
 
 // El mensaje [FAIL] escrito en rojo.
@@ -25,64 +25,73 @@ uint64_t failed_tests = 0;
 // El mensaje [SKIP] escrito en magenta.
 #define SKIP "[\033[95;1mSKIP\033[0m] "
 
-uint32_t fun_ej_3(uint32_t a, uint32_t b) {
-    uint32_t res = (a + 1) * b + 10;
-    return res;
+uint32_t fun_ej_3(uint32_t a, uint32_t b)
+{
+	uint32_t res = (a + 1) * b + 10;
+	return res;
 }
 
-void testResultMessage(bool comparacion, char* testName){
-	if (comparacion) {
+void testResultMessage(bool comparacion, char *testName)
+{
+	if (comparacion)
+	{
 		successful_tests++;
-		printf(DONE "%s: el resultado esperado y el obtenido coinciden.\n",testName);
-	} else {
-		failed_tests++;		
+		printf(DONE "%s: el resultado esperado y el obtenido coinciden.\n", testName);
+	}
+	else
+	{
+		failed_tests++;
 		printf(FAIL "%s: el resultado esperado y el obtenido no son iguales.\n", testName);
 	}
 }
 
-void test_1_sumas(){
-	uint64_t resultado_suma = ejercicio1(0,0,0,0,0);
+void test_1_sumas()
+{
+	uint64_t resultado_suma = ejercicio1(0, 0, 0, 0, 0);
 	testResultMessage(0 == resultado_suma, "test 1.1");
-	
-	resultado_suma = ejercicio1(75,0,0,0,0);
+
+	resultado_suma = ejercicio1(75, 0, 0, 0, 0);
 	testResultMessage(75 == resultado_suma, "test 1.2");
 
-	resultado_suma = ejercicio1(184,0,25,0,0);
+	resultado_suma = ejercicio1(184, 0, 25, 0, 0);
 	testResultMessage(209 == resultado_suma, "test 1.3");
 
-	resultado_suma = ejercicio1(5,0,10,5,0);
+	resultado_suma = ejercicio1(5, 0, 10, 5, 0);
 	testResultMessage(20 == resultado_suma, "test 1.4");
 
-	resultado_suma = ejercicio1(1,2,220,7,0);
+	resultado_suma = ejercicio1(1, 2, 220, 7, 0);
 	testResultMessage(230 == resultado_suma, "test 1.5");
 
-	resultado_suma = ejercicio1(100,101,102,103,104);
+	resultado_suma = ejercicio1(100, 101, 102, 103, 104);
 	testResultMessage(510 == resultado_suma, "test 1.6");
 
-	resultado_suma = ejercicio1(4294967299,1,20,1,21);
+	resultado_suma = ejercicio1(4294967299, 1, 20, 1, 21);
 	testResultMessage(4294967342 == resultado_suma, "test 1.7");
 
-	resultado_suma = ejercicio1(4294967295,4294967295,4294967295,4294967295,4294967295);
+	resultado_suma = ejercicio1(4294967295, 4294967295, 4294967295, 4294967295, 4294967295);
 	testResultMessage(21474836475 == resultado_suma, "test 1.8");
 }
 
-void test_ej_1() {
+void test_ej_1()
+{
 	uint64_t successful_at_start = successful_tests;
 	uint64_t failed_at_start = failed_tests;
 
-	if (!EJERCICIO_1_HECHO) {
+	if (!EJERCICIO_1_HECHO)
+	{
 		printf(SKIP "El ejercicio 1 no está hecho aún.\n");
 		return;
 	}
-	
+
 	test_1_sumas();
-	if (failed_at_start < failed_tests) {
+	if (failed_at_start < failed_tests)
+	{
 		printf(FAIL "El ejercicio 1 tuvo tests que fallaron.\n");
 	}
 }
 
-
-bool items_iguales(item_t* a, item_t* b){
+bool items_iguales(item_t *a, item_t *b)
+{
 	bool son_iguales = true;
 	son_iguales = son_iguales && (a->cantidad == b->cantidad);
 	son_iguales = son_iguales && (a->id == b->id);
@@ -90,43 +99,47 @@ bool items_iguales(item_t* a, item_t* b){
 	return son_iguales;
 }
 
-void test_2_struct(){
-	item_t* un_item = malloc(sizeof(item_t));
-	item_t* esperado = malloc(sizeof(item_t));
-	
+void test_2_struct()
+{
+	item_t *un_item = malloc(sizeof(item_t));
+	item_t *esperado = malloc(sizeof(item_t));
+
 	strcpy(esperado->nombre, "poncho");
 	esperado->id = 1234;
 	esperado->cantidad = 3;
 
 	ejercicio2(un_item, 1234, 3, "poncho");
-	
+
 	testResultMessage(items_iguales(esperado, un_item), "test 2.1");
 
 	free(un_item);
 	free(esperado);
 }
 
-void test_2_struct_borde(){
-	item_t* un_item = malloc(sizeof(item_t));
-	item_t* esperado = malloc(sizeof(item_t));
-	
+void test_2_struct_borde()
+{
+	item_t *un_item = malloc(sizeof(item_t));
+	item_t *esperado = malloc(sizeof(item_t));
+
 	strcpy(esperado->nombre, "cortinas");
 	esperado->id = 4294967295;
 	esperado->cantidad = 4294967295;
 
 	ejercicio2(un_item, 4294967295, 4294967295, "cortinas");
-	
+
 	testResultMessage(items_iguales(esperado, un_item), "test 2.2");
 
 	free(un_item);
 	free(esperado);
 }
 
-void test_ej_2() {
+void test_ej_2()
+{
 	uint64_t successful_at_start = successful_tests;
 	uint64_t failed_at_start = failed_tests;
-	
-	if (!EJERCICIO_2_HECHO) {
+
+	if (!EJERCICIO_2_HECHO)
+	{
 		printf(SKIP "El ejercicio 2 no está hecho aún.\n");
 		return;
 	}
@@ -134,43 +147,49 @@ void test_ej_2() {
 	test_2_struct();
 	test_2_struct_borde();
 
-	if (failed_at_start < failed_tests) {
+	if (failed_at_start < failed_tests)
+	{
 		printf(FAIL "El ejercicio 2 tuvo tests que fallaron.\n");
 	}
 }
 
-
-void test_3_vacio(){
+void test_3_vacio()
+{
 	uint32_t res = 0;
 	res = ejercicio3(NULL, 0, fun_ej_3);
 	testResultMessage(64 == res, "test 3.1");
 }
 
-void test_3_un_elemento(){
+void test_3_un_elemento()
+{
 	uint32_t res = 0;
 	uint32_t array[] = {2};
 	res = ejercicio3(array, 1, fun_ej_3);
 	testResultMessage(12 == res, "test 3.2");
 }
 
-void test_3_dos_elementos(){
+void test_3_dos_elementos()
+{
 	uint32_t res = 0;
-	uint32_t array[] = {2,3};
+	uint32_t array[] = {2, 3};
 	res = ejercicio3(array, 2, fun_ej_3);
 	testResultMessage(61 == res, "test 3.3");
 }
 
-void test_3_cinco_elementos(){
+void test_3_cinco_elementos()
+{
 	uint32_t res = 0;
-	uint32_t array[] = {1,1,1,1,1};
+	uint32_t array[] = {1, 1, 1, 1, 1};
 	res = ejercicio3(array, 5, fun_ej_3);
 	testResultMessage(341 == res, "test 3.4");
 }
 
-void test_ej_3(){
+void test_ej_3()
+{
 	uint64_t successful_at_start = successful_tests;
 	uint64_t failed_at_start = failed_tests;
-	if (!EJERCICIO_3_HECHO) {
+	if (!EJERCICIO_3_HECHO)
+	{
 		printf(SKIP "El ejercicio 3 no está hecho aún.\n");
 		return;
 	}
@@ -180,24 +199,28 @@ void test_ej_3(){
 	test_3_dos_elementos();
 	test_3_cinco_elementos();
 
-	if (failed_at_start < failed_tests) {
+	if (failed_at_start < failed_tests)
+	{
 		printf(FAIL "El ejercicio 3 tuvo tests que fallaron.\n");
 	}
 }
 
-void testResults4(uint32_t* esperado, uint32_t* resultado, uint32_t* inicial[], uint32_t size, char* testName) {
-	bool iguales =true;
+void testResults4(uint32_t *esperado, uint32_t *resultado, uint32_t *inicial[], uint32_t size, char *testName)
+{
+	bool iguales = true;
 
 	for (uint32_t i = 0; i < size; i++)
 	{
-		if(esperado[i] != resultado[i]){
+		if (esperado[i] != resultado[i])
+		{
 			iguales = false;
 		}
 	}
-	
+
 	for (uint32_t i = 0; i < size; i++)
 	{
-		if(inicial[i] != NULL){
+		if (inicial[i] != NULL)
+		{
 			iguales = false;
 		}
 	}
@@ -205,52 +228,58 @@ void testResults4(uint32_t* esperado, uint32_t* resultado, uint32_t* inicial[], 
 	testResultMessage(iguales, testName);
 }
 
+void test_4_un_elemento()
+{
+	uint32_t *array_inicial[1];
 
-void test_4_un_elemento(){
-	uint32_t* array_inicial[1];
-
-	uint32_t* punteroUint = malloc(sizeof(uint32_t));
+	uint32_t *punteroUint = malloc(sizeof(uint32_t));
 	*punteroUint = 10;
 	array_inicial[0] = punteroUint;
-	
+
 	uint32_t array_res[] = {80};
-	uint32_t* array = ejercicio4(array_inicial, 1, 8);
+	uint32_t *array = ejercicio4(array_inicial, 1, 8);
 	testResults4(array_res, array, array_inicial, 1, "test 4.1");
 	free(array);
 }
 
-void test_4_tres_elementos(){
-	uint32_t* array_inicial[3];
+void test_4_tres_elementos()
+{
+	uint32_t *array_inicial[3];
 
-	for(int i=0; i<3; i++){
-		uint32_t* punteroUint = malloc(sizeof(uint32_t));
-		*punteroUint = (i+1)*5;
+	for (int i = 0; i < 3; i++)
+	{
+		uint32_t *punteroUint = malloc(sizeof(uint32_t));
+		*punteroUint = (i + 1) * 5;
 		array_inicial[i] = punteroUint;
 	}
-	
-	uint32_t array_res[] = {25,50,75};
-	uint32_t* array = ejercicio4(array_inicial, 3, 5);
+
+	uint32_t array_res[] = {25, 50, 75};
+	uint32_t *array = ejercicio4(array_inicial, 3, 5);
 	testResults4(array_res, array, array_inicial, 3, "test 4.2");
 	free(array);
 }
 
-void test_ej_4(){
+void test_ej_4()
+{
 	uint64_t successful_at_start = successful_tests;
 	uint64_t failed_at_start = failed_tests;
-	if (!EJERCICIO_4_HECHO) {
+	if (!EJERCICIO_4_HECHO)
+	{
 		printf(SKIP "El ejercicio 4 no está hecho aún.\n");
 		return;
 	}
 
-    test_4_un_elemento();
+	test_4_un_elemento();
 	test_4_tres_elementos();
 
-	if (failed_at_start < failed_tests) {
+	if (failed_at_start < failed_tests)
+	{
 		printf(FAIL "El ejercicio 4 tuvo tests que fallaron.\n");
 	}
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 	test_ej_1();
 	test_ej_2();
 	test_ej_3();
@@ -258,12 +287,14 @@ int main(int argc, char* argv[]) {
 
 	printf(
 		"\nSe corrieron %lu tests. %lu corrieron exitosamente. %lu fallaron.\n",
-		failed_tests + successful_tests, successful_tests, failed_tests
-	);
+		failed_tests + successful_tests, successful_tests, failed_tests);
 
-	if (failed_tests || !EJERCICIO_1_HECHO || !EJERCICIO_2_HECHO || !EJERCICIO_3_HECHO || !EJERCICIO_4_HECHO) {
+	if (failed_tests || !EJERCICIO_1_HECHO || !EJERCICIO_2_HECHO || !EJERCICIO_3_HECHO || !EJERCICIO_4_HECHO)
+	{
 		return 1;
-	} else {
+	}
+	else
+	{
 		return 0;
 	}
 }
